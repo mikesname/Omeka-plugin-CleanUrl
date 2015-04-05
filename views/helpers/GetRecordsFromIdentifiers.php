26ca753cb645557ceb40cd966b5b99c83c0abf57
Daniel Berthereau <Daniel.github@Berthereau.net>
2015-04-06 00:00:00 +0200

    Added an argument to check record type in getRecordsFromIdentifiers().

--- views/helpers/GetRecordsFromIdentifiers.php
+++ views/helpers/GetRecordsFromIdentifiers.php
@@ -20,8 +20,9 @@ class CleanUrl_View_Helper_GetRecordsFromIdentifiers extends Zend_View_Helper_Ab
      * @param string|array $identifiers One or a list of identifiers. May
      * contains the prefix.
      * @param boolean $withPrefix Indicates if identifier contains the prefix.
+     * @param string $recordType Optional. Search a specific record type if any.
      * @param string $return Format of the returned value. can be: 'record',
-     * 'type and id' or 'id'.
+     * 'type and id' or 'id' (to use only when the type is known).
      * @param boolean $checkPublic Return results depending on users (default)
      * or not. If return format is 'record', check is always done. For files,
      * they are returned only for admin.
@@ -32,6 +33,7 @@ class CleanUrl_View_Helper_GetRecordsFromIdentifiers extends Zend_View_Helper_Ab
     public function getRecordsFromIdentifiers(
         $identifiers,
         $withPrefix = true,
+        $recordType = null,
         $return = 'record',
         $checkPublic = true
     ) {
@@ -53,7 +55,16 @@ class CleanUrl_View_Helper_GetRecordsFromIdentifiers extends Zend_View_Helper_Ab
 
         $elementId = (integer) get_option('clean_url_identifier_element');
 
+        if ($recordType) {
+            $sqlRecordType = "AND element_texts.record_type = ?";
+            $bind[] = $recordType;
+        }
+        else {
+            $sqlRecordType = '';
+        }
+
         // TODO Use filters for user or use regular methods (get_record_by_id() checks it).
+        // TODO Check if file is public here, when needed (currently, this is done after).
         if (($checkPublic || $return == 'record') && !(is_admin_theme() || current_user())) {
             $sqlFromIsPublic = "
                 LEFT JOIN {$db->Item} items
@@ -70,7 +81,7 @@ class CleanUrl_View_Helper_GetRecordsFromIdentifiers extends Zend_View_Helper_Ab
             $sqlWhereIsPublic = "
                 AND ((element_texts.record_type = 'Item' AND items.public = 1)
                     OR (element_texts.record_type = 'Collection' AND collections.public = 1)
-                    OR (element_texts.record_type = 'File' AND items.public = 1)
+                    OR (element_texts.record_type = 'File')
                 )
             ";
         }
@@ -102,10 +113,11 @@ class CleanUrl_View_Helper_GetRecordsFromIdentifiers extends Zend_View_Helper_Ab
         $sqlLimit = $one ? 'LIMIT 1' : '';
 
         $sql = "
-            SELECT element_texts.record_type as 'type', element_texts.record_id as 'id'
+            SELECT element_texts.record_type, element_texts.record_id
             FROM {$db->ElementText} element_texts
                 $sqlFromIsPublic
             WHERE element_texts.element_id = '$elementId'
+                $sqlRecordType
                 $sqlWhereText
                 $sqlWhereIsPublic
             ORDER BY
@@ -117,22 +129,28 @@ class CleanUrl_View_Helper_GetRecordsFromIdentifiers extends Zend_View_Helper_Ab
         if ($results) {
             switch ($return) {
                 case 'record':
+                default:
                     foreach ($results as $key => $result) {
-                        $results[$key] = get_record_by_id($result['type'], $result['id']);
+                        $results[$key] = get_record_by_id($result['record_type'], $result['record_id']);
                     }
+                    // Remove private files (empty now) if needed.
+                    $results = array_filter($results);
                     break;
 
                 case 'type and id':
-                    break;
-
-                case 'id':
-                    foreach ($results as $key => $result) {
-                        $results[$key] = $result['id'];
+                    if ($checkPublicFile) {
+                        $results = array_filter($results, 'self::_removePrivateFile');
                     }
                     break;
 
-                default:
-                    return null;
+                case 'id':
+                    if ($checkPublicFile) {
+                        $results = array_filter($results, 'self::_removePrivateFile');
+                    }
+                    foreach ($results as $key => $result) {
+                        $results[$key] = $result['record_id'];
+                    }
+                    break;
             }
             return $one
                 ? array_shift($results)
@@ -154,5 +172,21 @@ class CleanUrl_View_Helper_GetRecordsFromIdentifiers extends Zend_View_Helper_Ab
     private static function _addPrefixSpaceToIdentifier($string)
     {
         return self::$_prefix . ' ' . $string;
+    }
+
+    /**
+     * Return false if a record is a private file.
+     *
+     * @todo Check public file via a direct query in getRecordFromIdentifier().
+     * @param record $id File id to check.
+     * @return boolean
+     */
+    private static function _removePrivateFile($record)
+    {
+        if ($record['record_type'] == 'File') {
+            return (boolean) get_record_by_id('File', $record['record_id']);
+        }
+
+        return true;
     }
 }
